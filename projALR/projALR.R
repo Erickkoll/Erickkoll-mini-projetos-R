d7ee03afb37ce20c0ecfd82252be3cb7e4d90201
Erick Cordeiro Kollross <erickkollross@gmail.com>
2023-07-28 11:38:33 -0300

    Respondendo novas perguntas de negócio e corrigindo análise do diagnóstico dada novas definições do problema de negócio

--- projALR/projALR.R
+++ projALR/projALR.R
@@ -103,26 +103,45 @@ menor_idade
   
 #Qual grupo baseado em diagnóstico(APRDRG) tem o maior gasto no hospital?
 
-quantiles <- quantile(dados$APRDRG, probs = seq(0, 1, by = 0.2))
-dados$CLUSTER <- as.integer(cut(dados$APRDRG, breaks = 5, include.lowest = TRUE, labels = FALSE))
-
-View(dados)
-
-grupo_APRDRG_gasto <- sqldf("SELECT CLUSTER AS grupo,
+grupo_APRDRG_gasto <- sqldf("SELECT APRDRG AS grupo,
                             SUM(TOTCHG) AS valor
-                            FROM dados")
-grupo_APRDRG_gasto
+                            FROM dados
+                            GROUP BY APRDRG
+                            ORDER BY valor DESC
+                            LIMIT 1")
 
-def_grupo_3 <- sqldf("SELECT APRDRG AS dianostico
-                     FROM dados
-                     WHERE CLUSTER == 3")
-def_grupo_3
+#A raça do paciente tem relação com os gastos hosítalares?
+
+# Usaremos um Teste ANOVA.
+# Variável dependente no Teste ANOVA: TOTCHG
+# Variável independente no Teste ANOVA: Race
+
+# H0: Não há efeito de RACE em TOTCHG.
+# H1: Há efeito de RACE em TOTCHG.
+
+cor(dados$RACE, dados$TOTCHG, method = "pearson")
+summary(dados$RACE)
+summary(as.factor(dados$RACE))
+modelo_anova_1 <- aov(TOTCHG ~ RACE, data = dados)
+summary(modelo_anova_1)
+
+# Resposta da Pergunta 4:
+# O valor-p é maior que 0.05. Falhamos em rejeitar a H0.
+# A raça do paciente não influencia no gasto total com internação no hospital.
+
 
 #A combinação de idade e gênero dos pacientes influencia no gasto total em internações no hospital?
 
+modelo_anova_2 <- aov(TOTCHG ~ AGE + FEMALE, data = dados)
+summary(modelo_anova_2)
 
+# Resposta da Pergunta 4:
+# O valor-p é menor que 0.05. Reheitamos a H0.
+# A idade e o sexo influencia no gasto total com internação no hospital.
 
 #Como o tempo de permanência é o fator crucial para paciente internados, 
 #desejamos descobrir se o tempo de permanência pode ser previsto a partir da idade, gênero e raça
+
+
 
 #Quais variável tem mais impacto nos custos de internação hospitalar?
